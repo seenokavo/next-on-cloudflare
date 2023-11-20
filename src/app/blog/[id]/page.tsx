26ba96e56e2d0dab2e0dd135a7fc409164f1523f
Matej Alternative <alt.matej@gmail.com>
2023-11-20 11:09:24 +0100

    ClientSidePostFetch

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,16 +1,23 @@
+import {Suspense} from "react";
+import ClientSidePostFetch from "@/app/blog/[id]/ClientSidePostFetch";
+
 export const runtime = 'edge';
 
 interface Props {
     params: { id: string };
 }
 
-export default function ArticlePage({ params }: Props) {
+export default function ArticlePage({params}: Props) {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     This is a blog page for [{params.id}]
                 </p>
+
+                <Suspense>
+                    <ClientSidePostFetch id={params.id}/>
+                </Suspense>
             </div>
         </main>
     )
