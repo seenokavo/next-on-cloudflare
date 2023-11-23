e5787fc6a7a4cd955f7d129c87db4abf76255355
Matej Alternative <alt.matej@gmail.com>
2023-11-24 00:49:07 +0100

    Coloring Sparkline chart

--- src/app/components/grid-columns/chart-column/ClientChart.tsx
+++ src/app/components/grid-columns/chart-column/ClientChart.tsx
@@ -5,12 +5,17 @@ import {SparkLineChart} from '@mui/x-charts';
 
 interface Props {
     data: [number, number][];
+    color: 'red' | 'green';
 }
 
-export function ClientChart({data}: Props) {
+const green: string = 'rgb(76, 175, 80)';
+const red: string = 'rgb(244, 67, 54)';
+
+export function ClientChart({data, color}: Props) {
+    const targetColor = color === 'red' ? red : green;
     return (
         <div>
-            <SparkLineChart data={data.map(d => d[1])} width={140} height={40}/>
+            <SparkLineChart data={data.map(d => d[1])} width={140} height={40} colors={[targetColor]}/>
         </div>
     );
 }
--- src/app/components/grid-columns/chart-column/ColumnChart.tsx
+++ src/app/components/grid-columns/chart-column/ColumnChart.tsx
@@ -8,10 +8,12 @@ interface Props {
 
 export async function ColumnChart({symbol}: Props) {
     const data: ChartData = await fetch(`https://coincheckup.com/api/coincheckup/get_coin_charts?charts=7D&samples=48&coins=${symbol}`).then(res => res.json());
+    const series = data[symbol]['7D'];
+    const isPositive: boolean = series[0][1] <= series[series.length - 1][1];
 
     return (
         <div className={styles.container}>
-            <ClientChart data={data[symbol]['7D']}/>
+            <ClientChart data={series} color={isPositive ? 'green' : 'red'}/>
         </div>
     );
 }
