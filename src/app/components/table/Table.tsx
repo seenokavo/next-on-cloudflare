4e90f4845dbc180b1169a3ea4c07f69a0e8a527d
Matej Alternative <alt.matej@gmail.com>
2023-11-25 20:58:08 +0100

    HeaderRow

--- src/app/components/table/Table.tsx
+++ src/app/components/table/Table.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GridRow from '@/app/components/grid-row/GridRow';
 import {CoinListEntry} from '@/model/get-coin-list';
 import styles from './Table.module.css';
+import HeaderRow from '@/app/components/grid-columns/header-row/HeaderRow';
 
 interface Props {
     coinList: CoinListEntry[];
@@ -10,6 +11,7 @@ interface Props {
 export default function Table(props: Props) {
     return (
         <div className={styles.container}>
+            <HeaderRow/>
 
             {/*https://stackoverflow.com/questions/24284510/auto-adjust-perant-div-width-to-sum-of-its-children-width*/}
             <div className={styles.rowsWrapper}>
